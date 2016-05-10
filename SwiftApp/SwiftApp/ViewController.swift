073356e5d90a7d1d42213b51e4a2a4c091824e30
trace`sMacMini <luotianjia@126.com>
2016-05-10 14:25:18 +0800

    create swift empty project
    step 1

---
+++ SwiftApp/SwiftApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftApp
+//
+//  Created by 罗田佳 on 16/5/10.
+//  Copyright © 2016年 trace990. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
